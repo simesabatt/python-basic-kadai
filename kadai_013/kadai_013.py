8c8a39a2943e951902842e157aff58ca2f9eb58c
しめさばさん <t95203631@gmail.com>
2023-12-02 17:56:13 +0900

    Add files via upload

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,7 +1,7 @@
-def getPriceWithTax(price, tax_rate = 10):
+def getPriceWithTax(price, tax_rate):
   tax_rate = tax_rate / 100
   return price * (1 + tax_rate)
 
-print(getPriceWithTax(100))
+print(getPriceWithTax(100, 10))
 print(getPriceWithTax(100, 20))
 print(getPriceWithTax(500, 4))
